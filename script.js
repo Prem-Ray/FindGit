8a065a8e26cf1dcd8f915d58204f8eac1b14c3c5
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-07-09 15:44:46 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,196 @@
+// let input = document.querySelector('input');
+// let search = document.querySelector('button');
+// let image = document.querySelector('.logo');
+// let ProfileName = document.querySelector('#name');
+// let joiningDate = document.querySelector('#joiningDate');
+// let userName = document.querySelector('#username');
+// let about = document.querySelector('#about');
+// let reposval = document.querySelector('#reposval');
+// let followersval = document.querySelector('#followersval');
+// let followingval = document.querySelector('#followingval');
+// let city = document.querySelector('#city');
+// let twitter = document.querySelector('#twitter');
+// let blog = document.querySelector('#blog');
+// let linkedIn = document.querySelector('#linkedIn');
+
+// let url = "https://api.github.com/users";
+
+// let setdarklightmode = document.querySelector('.setdarklightmode');
+// let light = 1;
+
+// document.querySelector('.dev-container').classList.remove('active');
+
+// // get data from search
+// async function datasearch(api) {
+//     let response = await fetch(api);
+//     let val = await response.json();
+//     console.log(val);
+//     if (input.value === '') {
+//         document.querySelector('.dev-container').classList.remove('active');
+//         document.querySelector('.not-found').classList.add('active');
+//         return;
+//     }
+//     document.querySelector('.not-found').classList.remove('active');
+//     document.querySelector('.dev-container').classList.add('active');
+
+//     ProfileName.innerText = val.name;
+//     userName.innerText = val.login;
+//     let timestamp = val.created_at;
+//     let [date, time] = timestamp.split('T');
+//     let [year, month, day] = date.split('-');
+//     joiningDate.innerText = `Joined ${day}/${month}/${year}`;
+//     image.src = val.avatar_url;
+//     about.innerText = `${val.bio} || ${val.company} || ${val.location}`;
+    
+//     if(val.location) {
+//         let [cityName] = val.location.split(',');
+//         city.innerText = cityName;
+//     } else {
+//         city.innerText = "Not Available";
+//     }
+
+//     if(val.twitter_username){
+//         twitter.innerText = val.twitter_username;
+//     } else{
+//         twitter.innerText = "Not Available" ;
+//     }
+    
+//     if(val.blog){
+//         blog.innerText = val.blog;
+//     } else {
+//         blog.innerText = "Not Available";
+//     }
+
+//     followersval.innerText = val.followers;
+//     followingval.innerText = val.following;
+//     reposval.innerText = val.public_repos;
+// }
+
+// // for searching
+// search.addEventListener('click', (e) => {
+//     console.log(input.value);
+//     let api = `${url}/${input.value}`;
+//     console.log(api);
+
+//     datasearch(api);
+// });
+
+// // Handle Enter key press for search
+// input.addEventListener('keypress', (e) => {
+//     if (e.key === 'Enter') {
+//         search.click();
+//     }
+// });
+
+// // for dark-light mode set up
+// setdarklightmode.addEventListener('click', (e) => {
+//     if (light === 1) {
+//         // light mode to dark mode
+//         setdarklightmode.innerHTML = `Light <img src="./image/light-svgrepo-com.svg" height="24" width="24" class="darklightimg" alt="">`;
+//         light = 0;
+//         document.querySelector('.wrapper').style = `
+//             overflow-x: hidden;
+//             overflow-y: hidden;
+//             height: 100vh;
+//             width: 100vw;
+//             background-color: #041822;
+//             position: relative;
+//         `;
+
+//         document.querySelector('#size').style = `
+//         color:White ;
+//         display:flex ;
+//         justify-content: space-between;
+//         width:950px ;
+//         margin:1.08rem ;
+//         background-color: #042525 ;`;
+
+//         document.querySelector('.search-container').style = `
+//         font-size:1.2rem ;
+//         position: relative;
+//         padding: 1.3rem 2.33rem ;
+//         color:white ;
+//         background-color: #042525 ;
+//         display: flex ;
+//         justify-content: space-between;
+//         align-items: center;
+//         border-radius: 12px;`;
+
+//         document.querySelector('.darklightimg').style = `
+//         color:white ; 
+//         margin-left:1rem ;`;
+
+//         document.querySelector('input').style = `
+//         background: none;
+//         border: none;
+//         color: white; 
+//         font-size:1.2rem ;
+//         font-weight:250 ;
+//         padding-left:8px ;`;
+
+//         document.querySelector('.dev-container').style = `
+//         width:950px ;
+//         color:#FFFFFF ;
+//         background-color: #042525 ;
+//         position: relative;
+//         height:405px ;
+//         border-radius: 12px ;`;
+        
+//     } else { 
+//         // dark mode to light mode
+//         setdarklightmode.innerHTML = `Dark <img src="./image/moon-dark-theme-svgrepo-com.svg" height="24" width="24" class="darklightimg" alt="">`;
+//         light = 1;
+
+//         document.querySelector('.wrapper').style = `
+//             overflow-x: hidden ;
+//             overflow-y: hidden ;
+//             height:100vh ;
+//             width:100vw ;
+//             background-color: #FFFFFF;
+//             position: relative;
+//         `;
+
+//         document.querySelector('#size').style = `
+//         color:black ;
+//         display:flex ;
+//         justify-content: space-between;
+//         width:950px ;
+//         margin:1.08rem ;
+//         background-color: #87CEEB;`;
+
+//         document.querySelector('.search-container').style = `
+//         font-size:1.2rem ;
+//         position: relative;
+//         padding: 1.3rem 2.33rem ;
+//         color:white ;
+//         background-color: #87CEEB;
+//         display: flex ;
+//         justify-content: space-between;
+//         align-items: center;
+//         border-radius: 12px;`;
+
+//         document.querySelector('.darklightimg').style = `
+//         color:#041822 ;
+//         margin-left:1rem ;`;
+
+//         document.querySelector('input').style = `
+//         background: none;
+//         border: none;
+//         color:#041822 ;
+//         font-size:1.2rem ;
+//         font-weight:250 ;
+//         padding-left:8px ;`;
+
+//         document.querySelector('.dev-container').style = `
+//         width:950px ;
+//         color:#212121 ;
+//         background-color: #fff;
+//         position: relative;
+//         height:405px ;
+//         border-radius: 12px ;`;
+//     }
+// });
+
 let input = document.querySelector('input');
 let search = document.querySelector('button');
 let image = document.querySelector('.logo');
@@ -13,180 +206,66 @@ let twitter = document.querySelector('#twitter');
 let blog = document.querySelector('#blog');
 let linkedIn = document.querySelector('#linkedIn');
 
+let setdarklightmode = document.querySelector('.setdarklightmode');
 let url = "https://api.github.com/users";
 
-let setdarklightmode = document.querySelector('.setdarklightmode');
-let light = 1;
+// Initialize light mode by default
+let light = true;
 
+// Hide profile container initially
 document.querySelector('.dev-container').classList.remove('active');
 
-// get data from search
+// Fetch GitHub data
 async function datasearch(api) {
-    let response = await fetch(api);
-    let val = await response.json();
-    console.log(val);
-    if (input.value === '') {
+    const response = await fetch(api);
+    const val = await response.json();
+
+    if (!input.value.trim() || val.message === "Not Found") {
         document.querySelector('.dev-container').classList.remove('active');
         document.querySelector('.not-found').classList.add('active');
         return;
     }
+
     document.querySelector('.not-found').classList.remove('active');
     document.querySelector('.dev-container').classList.add('active');
 
-    ProfileName.innerText = val.name;
-    userName.innerText = val.login;
-    let timestamp = val.created_at;
-    let [date, time] = timestamp.split('T');
+    ProfileName.innerText = val.name || "Not Available";
+    userName.innerText = val.login || "Not Available";
+    let [date] = val.created_at.split('T');
     let [year, month, day] = date.split('-');
     joiningDate.innerText = `Joined ${day}/${month}/${year}`;
     image.src = val.avatar_url;
-    about.innerText = `${val.bio} || ${val.company} || ${val.location}`;
+    about.innerText = `${val.bio || 'No bio'} || ${val.company || 'No company'} || ${val.location || 'No location'}`;
     
-    if(val.location) {
-        let [cityName] = val.location.split(',');
-        city.innerText = cityName;
-    } else {
-        city.innerText = "Not Available";
-    }
-
-    if(val.twitter_username){
-        twitter.innerText = val.twitter_username;
-    } else{
-        twitter.innerText = "Not Available" ;
-    }
-    
-    if(val.blog){
-        blog.innerText = val.blog;
-    } else {
-        blog.innerText = "Not Available";
-    }
+    city.innerText = val.location ? val.location.split(',')[0] : "Not Available";
+    twitter.innerText = val.twitter_username || "Not Available";
+    blog.innerText = val.blog || "Not Available";
 
     followersval.innerText = val.followers;
     followingval.innerText = val.following;
     reposval.innerText = val.public_repos;
 }
 
-// for searching
-search.addEventListener('click', (e) => {
-    console.log(input.value);
-    let api = `${url}/${input.value}`;
-    console.log(api);
-
+// Trigger search
+search.addEventListener('click', () => {
+    const api = `${url}/${input.value.trim()}`;
     datasearch(api);
 });
 
-// Handle Enter key press for search
+// Allow Enter key for search
 input.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         search.click();
     }
 });
 
-// for dark-light mode set up
-setdarklightmode.addEventListener('click', (e) => {
-    if (light === 1) {
-        // light mode to dark mode
-        setdarklightmode.innerHTML = `Light <img src="./image/light-svgrepo-com.svg" height="24" width="24" class="darklightimg" alt="">`;
-        light = 0;
-        document.querySelector('.wrapper').style = `
-            overflow-x: hidden;
-            overflow-y: hidden;
-            height: 100vh;
-            width: 100vw;
-            background-color: #041822;
-            position: relative;
-        `;
+// Dark/Light Mode Toggle
+setdarklightmode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    light = !light;
 
-        document.querySelector('#size').style = `
-        color:White ;
-        display:flex ;
-        justify-content: space-between;
-        width:950px ;
-        margin:1.08rem ;
-        background-color: #042525 ;`;
-
-        document.querySelector('.search-container').style = `
-        font-size:1.2rem ;
-        position: relative;
-        padding: 1.3rem 2.33rem ;
-        color:white ;
-        background-color: #042525 ;
-        display: flex ;
-        justify-content: space-between;
-        align-items: center;
-        border-radius: 12px;`;
-
-        document.querySelector('.darklightimg').style = `
-        color:white ; 
-        margin-left:1rem ;`;
-
-        document.querySelector('input').style = `
-        background: none;
-        border: none;
-        color: white; 
-        font-size:1.2rem ;
-        font-weight:250 ;
-        padding-left:8px ;`;
-
-        document.querySelector('.dev-container').style = `
-        width:950px ;
-        color:#FFFFFF ;
-        background-color: #042525 ;
-        position: relative;
-        height:405px ;
-        border-radius: 12px ;`;
-        
-    } else { 
-        // dark mode to light mode
-        setdarklightmode.innerHTML = `Dark <img src="./image/moon-dark-theme-svgrepo-com.svg" height="24" width="24" class="darklightimg" alt="">`;
-        light = 1;
-
-        document.querySelector('.wrapper').style = `
-            overflow-x: hidden ;
-            overflow-y: hidden ;
-            height:100vh ;
-            width:100vw ;
-            background-color: #FFFFFF;
-            position: relative;
-        `;
-
-        document.querySelector('#size').style = `
-        color:black ;
-        display:flex ;
-        justify-content: space-between;
-        width:950px ;
-        margin:1.08rem ;
-        background-color: #87CEEB;`;
-
-        document.querySelector('.search-container').style = `
-        font-size:1.2rem ;
-        position: relative;
-        padding: 1.3rem 2.33rem ;
-        color:white ;
-        background-color: #87CEEB;
-        display: flex ;
-        justify-content: space-between;
-        align-items: center;
-        border-radius: 12px;`;
-
-        document.querySelector('.darklightimg').style = `
-        color:#041822 ;
-        margin-left:1rem ;`;
-
-        document.querySelector('input').style = `
-        background: none;
-        border: none;
-        color:#041822 ;
-        font-size:1.2rem ;
-        font-weight:250 ;
-        padding-left:8px ;`;
-
-        document.querySelector('.dev-container').style = `
-        width:950px ;
-        color:#212121 ;
-        background-color: #fff;
-        position: relative;
-        height:405px ;
-        border-radius: 12px ;`;
-    }
+    setdarklightmode.innerHTML = light
+        ? `Dark <img src="./image/moon-dark-theme-svgrepo-com.svg" height="24" width="24" class="darklightimg" alt="">`
+        : `Light <img src="./image/light-svgrepo-com.svg" height="24" width="24" class="darklightimg" alt="">`;
 });
+
